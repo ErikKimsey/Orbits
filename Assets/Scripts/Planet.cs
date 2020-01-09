4107772f31a4df6dc02eb43b27fef0cc4445c6e0
Erik Kimsey <kimseyerik@gmail.com>
2020-01-09 11:32:08 -0500

    Created vars for semiMinor/Major axes and eccentricity

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -11,21 +11,42 @@ public class Planet : MonoBehaviour
     public GameObject m_StarGameObj;
     Star m_Star;
 
+    private Vector3 distFromStar;
+
+    private Vector3 currPos;
+
+    private float semiMinorAxis;
+    private float semiMajorAxis;
+
+    private float eccentricity;
+
+
     void Start()
     {
       planet = GetComponent<MeshRenderer>();
       planetPos = transform.position;
       m_StarGameObj = GameObject.Find("Star");
       m_Star = m_StarGameObj.GetComponent<Star>();
+      semiMajorAxis = 7.0f;
+      semiMinorAxis = 2.0f;
+      eccentricity = 0.7f;
+    }
 
-      Debug.Log(planet);
-      Debug.Log(planetPos);
-      Debug.Log(m_Star.GetPosition());
+    void CalculateDistance(){
+           if (m_Star)
+        {
+            float dist = Vector3.Distance(m_Star.GetPosition(), transform.position);
+            print("Distance to other: " + dist);
+        }
+    }
+
+    void CreateEccentricity(){
+      // 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      CalculateDistance();
     }
 }
