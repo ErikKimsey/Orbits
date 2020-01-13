25d591349b76bc262dd3bc853b04f86be2333e87
Erik Kimsey <kimseyerik@gmail.com>
2020-01-13 12:44:04 -0500

    Vars according to https://www.youtube.com/watch?v=GHc9RF258VA

--- Assets/Scripts/AudioRecorder.cs
+++ Assets/Scripts/AudioRecorder.cs
@@ -3,25 +3,78 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
+[RequireComponent (typeof(AudioSource))]
 public class AudioRecorder : MonoBehaviour {
   
   AudioSource m_AudioSrc;
 
-  /**
-   NEEDS:
-   - AudioClip,
-   - AudioSource,
-   - Microphone,
-   - bool permission,
-   - float[] for audioBuffer64, audioBand,
-   - bool if mic is detected (Microphone.devices.length > 0)
-  */
+  
+  //  NEEDS:
+  //  - AudioClip,
+  public AudioClip _audioClip;
+  public bool _useMic;
+  private bool _hasPermission;
+  //  - AudioSource,
+  //  - Microphone,
+
+  //  - - 
+  //  - float[] for: audioBandBuffer, audioBand,
+  private float[] _audioBand = new float[8];
+  private float[] _audioBandBuffer = new float[8];
+  //  - - 
+  //  - float[512] for: samplesLeft, samplesRight
+  private float[] _sampleLeft = new float[512];
+  private float[] _sampleRight = new float[512];
+  //  - - 
+  //  - float[8] for: freqBand, bandBuffer, bufferDecrease, freqBandPeak,
+  float[] _freqBand = new float[8];
+  float[] _bandBuffer = new float[8];
+  float[] _bufferDecrease = new float[8];
+  float[] _freqBandPeak = new float[8];
+  //  - -
+  //  - bool if mic is detected (Microphone.devices.length > 0),
+  private bool _micDetected;
+  //  - -
+  //  - public float _Amplitude, _AmplitudeBuffer,
+  //  - public float _AmplitudePeak,
+  //  - public float _audioProfile
+  //  - -
+  private float _Amplitude, _AmplitudeBuffer;
+  private float _AmplitudePeak;
+  private float _audioProfile;
+
+  //  - public enum _channel {Stereo, Left, Right}
+  //  - public _channel channel = new _channel();
+  private enum _Channel {Stereo, Left, Right};
+  private _Channel channel = new _Channel();
+  //  - -
+  // Audio64:
+  //  - freqBand64, bandBuffer64, bufferDecrease64, freqBandPeak64, audioBandBuffer64, audioBand64
+  private float[] _freqBand64 = new float[64];
+  private float[] _bandBuffer64 = new float[64];
+  private float[] _bufferDecrease64 = new float[64];
+  private float[] _freqBandPeak64 = new float[64];
+
+  private float[] _audioBand64 = new float[64];
+  private float[] _audioBandBuffer64 = new float[64];
+
+
+
   private void Start() {
     m_AudioSrc = GetComponent<AudioSource>();
-    m_AudioSrc.clip = Microphone.Start("Built-In Microphone", true, 30, AudioSettings.outputSampleRate);
     m_AudioSrc.loop = true;
     while(!(Microphone.GetPosition(null) > 0)){}
-    m_AudioSrc.Play();
+    // m_AudioSrc.Play();
+  }
+
+  private void GetPermission(){
+
+  }
+
+  private void GetMic(){
+    if(Microphone.devices.ToString().Length > 0){
+      m_AudioSrc.clip = Microphone.Start("Built-In Microphone", true, 30, AudioSettings.outputSampleRate);
+    }
   }
 
   /**
